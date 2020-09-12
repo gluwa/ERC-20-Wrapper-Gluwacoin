0dd17471f767960d4f8cbea6def5acb212eae15b
Tae Oh <taelimoh@gmail.com>
2020-09-12 20:12:15 +0900

    init Gluwacoin

--- contracts/Gluwacoin.sol
+++ contracts/Gluwacoin.sol
@@ -1,17 +1,18 @@
 pragma solidity ^0.6.2;
 
+import "./ERC20.sol";
+
 // Import the IERC20 interface and and SafeMath library
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
 
 /**
  * @dev Generic ERC20 token.
  *
- * Step 1 - ERC20 *
- * Step 2 - Gluwacoin
+ * Step 1 - ERC20
+ * Step 2 - Gluwacoin *
  * Step 3 - ERC20-Wrapper Gluwacoin
  */
-contract ERC20 is Initializable, ERC20UpgradeSafe {
+contract Gluwacoin is Initializable, ERC20 {
 
     // Reserved storage space to allow for layout changes in the future.
     // https://github.com/OpenZeppelin/openzeppelin-sdk/issues/912
