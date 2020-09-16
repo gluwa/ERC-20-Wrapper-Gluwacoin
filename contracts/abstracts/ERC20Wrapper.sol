2dcee0b095403b039ed3c01c5b091a9283be8b20
Tae Oh <taelimoh@gmail.com>
2020-09-17 01:30:33 +0900

    Update ERC20Wrapper.sol
    remove ETHless burn

--- contracts/abstracts/ERC20Wrapper.sol
+++ contracts/abstracts/ERC20Wrapper.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.6.2;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
@@ -11,14 +10,9 @@ import "../Validate.sol";
  * @dev Extension of {ERC20} that allows a certain ERC20 token holders to wrap the token to mint this token.
  * Holder of this token can retrieve the wrapped token by burning this token.
  */
-abstract contract ERC20WrapperUpgradeSafe is Initializable, AccessControlUpgradeSafe, ERC20UpgradeSafe {
+abstract contract ERC20WrapperUpgradeSafe is Initializable, ERC20UpgradeSafe {
     // base token, the token to be wrapped
     IERC20 private _token;
-
-    mapping (address => mapping (uint256 => bool)) private _usedNonces;
-
-    // collects burn relay fee
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     event Mint(address indexed _mintTo, uint256 _value);
     event Burnt(address indexed _burnFrom, uint256 _value);
@@ -74,50 +68,6 @@ abstract contract ERC20WrapperUpgradeSafe is Initializable, AccessControlUpgrade
         _burn(_msgSender(), amount);
 
         emit Burnt(_msgSender(), amount);
-    }
-
-    /**
-     * @dev ETHless {ERC20-_burn}.
-     * Destroys `amount` tokens from the `sender`'s account
-     * and moves `fee` tokens from the `sender`'s account to a burner's account.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits two {Transfer} events.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the `sender` must have a balance of at least the sum of `amount` and `fee`.
-     * - the `nonce` is only used once per `sender`.
-     */
-    function burn(address sender, address recipient, uint256 amount, uint256 fee, uint256 nonce, bytes memory sig)
-    public {
-        _useNonce(sender, nonce);
-
-        Validate.validateSignature(address(this), sender, recipient, amount, fee, nonce, sig);
-
-        _collect(sender, fee);
-        _burn(sender, amount);
-
-        emit Burnt(sender, amount);
-    }
-
-    /* @dev Uses `nonce` for the signer.
-    */
-    function _useNonce(address signer, uint256 nonce) private {
-        require(!_usedNonces[signer][nonce], "ERC20Wrapper: the nonce has already been used for this address");
-        _usedNonces[signer][nonce] = true;
-    }
-
-    /** @dev Collects `fee` from the sender.
-     *
-     * Emits a {Transfer} event.
-     */
-    function _collect(address sender, uint256 amount) internal {
-        address burner = getRoleMember(BURNER_ROLE, 0);
-
-        _transfer(sender, burner, amount);
     }
 
     uint256[50] private __gap;
