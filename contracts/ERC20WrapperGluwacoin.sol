512c1ad1a7c3f6caee089a8285fc89e2de15b7dd
Tae Oh <taelimoh@gmail.com>
2020-11-26 00:52:35 +0900

    Update ERC20WrapperGluwacoin.sol

--- contracts/ERC20WrapperGluwacoin.sol
+++ contracts/ERC20WrapperGluwacoin.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/Context.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Pausable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
 
 import "./abstracts/ERC20ETHlessTransfer.sol";
@@ -13,7 +14,7 @@ import "./abstracts/ERC20Wrapper.sol";
  * @dev Extension of {Gluwacoin} that allows a certain ERC20 token holders to wrap the token to mint this token.
  * Holder of this token can retrieve the wrapped token by burning this token.
  */
-contract ERC20WrapperGluwacoin is Initializable, ContextUpgradeSafe, ERC20ETHless, ERC20Reservable, ERC20Wrapper  {
+contract ERC20WrapperGluwacoin is Initializable, ContextUpgradeSafe, ERC20ETHless, ERC20Reservable, ERC20Wrapper, ERC20PausableUpgradeSafe  {
     // note that `decimals` must match that of `token` or less
     function initialize(string memory name, string memory symbol, uint8 decimals, IERC20 token) public {
         __ERC20Wrapper_init(name, symbol, decimals, token);
@@ -33,7 +34,7 @@ contract ERC20WrapperGluwacoin is Initializable, ContextUpgradeSafe, ERC20ETHles
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override
-    (ERC20UpgradeSafe, ERC20Reservable) {
+    (ERC20UpgradeSafe, ERC20PausableUpgradeSafe, ERC20Reservable) {
         super._beforeTokenTransfer(from, to, amount);
     }
 
